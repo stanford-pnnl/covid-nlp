d17db8b40d6a261a4b7d811cceed4b1acd41636d
Colby Ham <colby.ham@pnnl.gov>
2020-08-26 23:46:55 +0000

    Update mental health analysis script for easy rsults printing

--- src/mental_health_analysis.py
+++ src/mental_health_analysis.py
@@ -1,4 +1,5 @@
 from patient_db import PatientDB, get_top_k
+import json
 from collections import Counter
 
 
@@ -9,14 +10,14 @@ def mental_health_co_morbidities(patients, search_terms):
     print(f"search_terms: {search_terms}")
     for term in search_terms:
         term_patients_matched, term_matches = \
-                patients.match_term(term, event_keys=['diagnosis_name'], event_types=['DiagnosisEvent'])
+                patients.match_patients(term, event_keys=['diagnosis_name'], event_types=['DiagnosisEvent'])
         patients_matched.merge_patients(term_patients_matched)
         print(f"\tterm: {term}, len(term_matches): {len(term_matches)}")
 
         matches = matches.union(term_matches)
     print(f"{len(patients_matched.patients)} patients matched")
 
-    # Lookup information from visits where these codes were reported and collect other diagnossis code
+    # Lookup information from visits where these codes were reported and collect other diagnosis code
     patient_ids = set()
     visit_ids = set()
     event_ids = set()
@@ -30,28 +31,33 @@ def mental_health_co_morbidities(patients, search_terms):
         keys.add(key)
         terms.add(term)
 
-    diagnosis_counter = patients_matched.get_diagnosis_event_info()
+    diagnosis_event_counters, diagnosis_event_roles, diagnosis_event_entity_levels, diagnosis_event_count_per_entity_level = \
+            patients_matched.get_event_counters(event_types=['DiagnosisEvent'])
+    for entity_level, entity_count in diagnosis_event_count_per_entity_level.items():
+        print(f"entity_level: {entity_level}, entity_count: {entity_count}")
 
     # Aggregate counts for each such diagnosis code either based on the number of visits or number of patients
     patient_matched_stats = patients_matched.get_stats()
     print("Report top-k diagnosis codes")
     # Report top-k diagnosis codes
-    k = 10
-    entity_levels = ['patient', 'visit', 'event']
-    diagnosis_event_roles = ['diagnosis_icd9', 'diagnosis_name']
-    top_k = get_top_k(diagnosis_counter, entity_levels, diagnosis_event_roles, k)
-    for entity_level in entity_levels:
-        print(f"Top {k} diagnosis codes per {entity_level}: {top_k[entity_level]}")
+    k, top_k = get_top_k(diagnosis_event_counters, diagnosis_event_entity_levels, diagnosis_event_roles, k=10)
+    for entity_level in top_k:
+        print(f"Top {k} diagnosis codes per {entity_level}:")
+        #{top_k[entity_level]}")
+        for key, values in top_k[entity_level].items():
+            print(f"\tkey: {key}\n\t\t{values}")
+        print()
     import pdb;pdb.set_trace()
 
 
 def main():
     data_dir = "/home/colbyham/covid-nlp/output"
-    patients_db_path = f"{data_dir}/patients_20200821-221919.jsonl"
+    patients_db_path = f"{data_dir}/patients_20200826-230117.jsonl"
 
     # Create and load an instance of PatientDB
     patients = PatientDB(name='all')
     patients.load(patients_db_path)
+    import pdb;pdb.set_trace()
 
     print("Question 1:")
     question_one_terms = ['Depression', 'Anxiety', 'Insomnia', 'Distress']
