e30a445a3cba52d616297be166f2a1841bcca7d2
Colby Ham <colby.ham@pnnl.gov>
2020-08-31 06:53:31 +0000

    Style formatting updates

--- src/mental_health_analysis.py
+++ src/mental_health_analysis.py
@@ -10,10 +10,13 @@ def mental_health_co_morbidities(patients, search_terms):
     print(f"search_terms: {search_terms}")
     for term in search_terms:
         term_patients_matched, term_matches = \
-                patients.match_patients(term, event_keys=['diagnosis_name'], event_types=['DiagnosisEvent'])
+                patients.match_patients(
+                        term,
+                        event_keys=['diagnosis_name'],
+                        event_types=['DiagnosisEvent'])
         patients_matched.merge_patients(term_patients_matched)
         print(f"\tterm: {term}, len(term_matches): {len(term_matches)}")
-
+    
         matches = matches.union(term_matches)
     print(f"{len(patients_matched.patients)} patients matched")
 
@@ -31,7 +34,10 @@ def mental_health_co_morbidities(patients, search_terms):
         keys.add(key)
         terms.add(term)
 
-    diagnosis_event_counters, diagnosis_event_roles, diagnosis_event_entity_levels, diagnosis_event_count_per_entity_level = \
+    diagnosis_event_counters, \
+            diagnosis_event_roles, \
+            diagnosis_event_entity_levels, \
+            diagnosis_event_count_per_entity_level = \
             patients_matched.get_event_counters(event_types=['DiagnosisEvent'])
     for entity_level, entity_count in diagnosis_event_count_per_entity_level.items():
         print(f"entity_level: {entity_level}, entity_count: {entity_count}")
@@ -49,10 +55,15 @@ def mental_health_co_morbidities(patients, search_terms):
         print()
     import pdb;pdb.set_trace()
 
+    print('Test split patient DB by monthly frequency key')
+    monthly_splits = patients_matched.agg_time(time_freq='M')
+
+    import pdb;pdb.set_trace()
+
 
 def main():
     data_dir = "/home/colbyham/covid-nlp/output"
-    patients_db_path = f"{data_dir}/patients_20200826-230117.jsonl"
+    patients_db_path = f"{data_dir}/patients_20200831-050502.jsonl"
 
     # Create and load an instance of PatientDB
     patients = PatientDB(name='all')
