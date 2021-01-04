5ca00d2e4ba335d4f13edf6c66ad109b4d2b5c86
Colby Ham <colby.ham@pnnl.gov>
2021-01-04 11:54:34 -0800

    Update to iterate over partitions and use script dir for simplicity

--- src/ingest_jsonl.py
+++ src/ingest_jsonl.py
@@ -1,12 +1,15 @@
 import json
+import os
 import sys
 from collections import Counter
-import os
+from pathlib import Path
+
 
 def dump_patient(patient, path):
     with open(path, 'a+') as fout:
         patient_str = json.dumps(patient)
         fout.write(f"{patient_str}\n")
+
 
 def main(input_path, output_path):
     # Remove output file
@@ -15,7 +18,6 @@ def main(input_path, output_path):
     except OSError:
         print(f"Can't delete {output_path} as it does not exist")
 
-    use_first_visit = True
     c = Counter()
     non_empty_section_headers = Counter()
     counter_num_visits = Counter()
@@ -23,9 +25,15 @@ def main(input_path, output_path):
     with open(input_path, 'r') as fin:
         for line in fin:
             line_num += 1
-            if line_num % 10000 == 0:
+            if line_num % 500 == 0:
                 print(f"Processing line no: {line_num}", flush=True)
-            patient = json.loads(line)
+            try:
+                patient = json.loads(line)
+            except json.JSONDecodeError:
+                c['JSONDecodeError'] += 1
+                print(f"line_num: {line_num} failed due to JSONDecodeError")
+                continue
+
             patient_id = patient['patient_id']
             c['total_patients'] += 1
             visits = patient['visits']
@@ -44,8 +52,12 @@ def main(input_path, output_path):
 
                 # Skip empty section data
                 non_empty_section_data = []
-                for section in visit['section_data']:
+                for section_i, section in enumerate(visit['section_data']):
                     c['total_sections'] += 1
+                    # Only use the first section
+                    if use_first_section and section_i != 0:
+                        continue
+
                     if not section['entity_extraction_results']:
                         c['empty_sections'] += 1
                         continue
@@ -68,7 +80,7 @@ def main(input_path, output_path):
             patient['patient_id'] = str(patient_id)
             dump_patient(patient, output_path)
             c['dumped_patients'] += 1
-    print()
+    print(c, flush=True)
 
 
 if __name__ == '__main__':
@@ -80,6 +92,33 @@ if __name__ == '__main__':
         repo_dir = '/home/colbyham/covid-nlp'
         base_path = 'covid_like_patients_entity'
 
-    input_path = f'{repo_dir}/input/{base_path}.jsonl'
-    output_path = f'{repo_dir}/output/{base_path}-first_visit.jsonl'
-    main(input_path, output_path)
+    n_partitions = 7
+    n_patients_per_partition = 1000
+    extension = 'jsonl'
+    script_name = 'ingest_jsonl'
+    input_dir = f'{repo_dir}/{script_name}/input'
+    output_dir = f'{repo_dir}/{script_name}/output'
+
+    # Make sure output directory is created
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
+
+    # only use the first visit
+    use_first_visit = False
+    # only use the first section for each visit
+    use_first_section = True
+
+    # Build output suffix based on bools
+    output_suffix = ""
+    if use_first_section:
+        output_suffix += "-first_section"
+    if use_first_visit:
+        output_suffix += "-first_visit"
+
+    for partition_i in range(n_partitions):
+        print(f"Processing partition {partition_i}")
+        input_path = f'{input_dir}/{base_path}.{partition_i}.{extension}'
+        output_path = f'{output_dir}/{base_path}{output_suffix}.' \
+            f'{partition_i}.{extension}'
+        print(f"\tinput_path: {input_path}", flush=True)
+        print(f"\toutput_path: {output_path}", flush=True)
+        main(input_path, output_path)
